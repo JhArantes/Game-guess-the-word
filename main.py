409173a945438c645bdf90682cd33d02df6755fa
JhArantes <joaohenriqueavieira@gmail.com>
2024-02-18 09:38:16 -0300

    18/02/24 Atualização Jogo palavra: Mais palavras, melhor trabalho do codigo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-palavra_secreta = ['p', 'e', 'r' ,'f' ,'u' ,'m' , 'e']
-palavra_user = ['*', '*','*' ,'*' ,'*' ,'*' ,'*']
+from random import choice #escolher lista
+import os #limpar terminal
+
+# Controle palavras
+list_secrets_words = ['apple','working', 'cavalo', 'computador']
+palavra_secreta = choice(list_secrets_words)
+user_letras = []
 
 
 print('-=' * 20)
@@ -7,23 +12,49 @@ print('Vamos jogar um jogo!!\nFale letras e direi se ela existe na palavra secre
 print('-=' * 20)
 
 
-import os
-# loop para caso o user erre  
+
 
 contador = 0
 while True:
-    #pega letra
-    letra_user = input("Digite uma letra: ").lower().strip()
+    contador += 1
     
+    #pega letra
+    letra_user = input("\nDigite uma letra: ").lower().strip()
+    
+    if letra_user == palavra_secreta:
+        break
     # Apenas 1 
     if len(letra_user) > 1:
         print('Passe apenas uma letra')
         continue
+    if letra_user in user_letras:
+        print('Está letra já foi digitada')
+        continue
     
     
+    user_letras.append(letra_user)
+    print('Secret Word: ', end='')
+    for i in palavra_secreta:
+        
+        if i in user_letras:
+            print(i, end='')
+        elif i not in user_letras:
+            print('*', end='')
+            
+    os.system('cls')
+
+os.system('cls')
+
+print('Você Ganhou!! com ', contador,' tentativas')
+
+
+
+'''
     for p, i in enumerate(palavra_secreta):
         if i == letra_user:
             palavra_user[p] = i
+        
+        
 
     visao_palavra = ''.join(palavra_user)
     print(visao_palavra)
@@ -31,6 +62,4 @@ while True:
     
     if palavra_secreta == palavra_user:
         break
-os.system('cls')
-
-print('Você Ganhou!! com ', contador,' tentativas')
+'''  
